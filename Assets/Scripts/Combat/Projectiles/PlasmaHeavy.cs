e616d910c28c392ccdccefb969df76627f19eaa4
avanillaslice <65110409+avanillaslice@users.noreply.github.com>
2024-06-12 18:40:25 +0800

    Added HeavyCanon Script and Potential Anim

---
+++ Assets/Scripts/Combat/Projectiles/PlasmaHeavy.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PlasmaHeavy : ProjectileBase
+{
+    protected override void InitializeBehaviour(float speedModifier, Vector2 initialVelocity, WeaponBase.RelativeSide side)
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        // Set the velocity of the projectile
+        rb.velocity = initialVelocity + (Vector2)(transform.up * BaseSpeed * speedModifier);
+    }
+}
