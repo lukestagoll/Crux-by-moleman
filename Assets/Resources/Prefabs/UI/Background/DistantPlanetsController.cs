aa81d9437a943dcadfd5e5e7d02892091302fd10
avanillaslice <65110409+avanillaslice@users.noreply.github.com>
2024-07-27 23:10:00 +0800

    Updated Background

--- Assets/Resources/Prefabs/UI/Background/DistantPlanetsController.cs
+++ Assets/Resources/Prefabs/UI/Background/DistantPlanetsController.cs
@@ -4,13 +4,13 @@ using System.Collections.Generic;
 public class DistantPlanetsController : MonoBehaviour, IBackgroundController
 {
     public float scrollSpeed; // Speed at which the objects scroll
-    public float upperYThreshold; // Y value to instantiate a new object
-    public float lowerYThreshold; // Y value to remove the old object
-    public float duration; // Duration to wait before checking positions again
-    private float zAxisValue = 25f; // Constant z-axis value
-    public float minDistanceBetweenObjects = 6f; // Minimum distance between objects
+    public float upperYThreshold = 3f; // Y value to instantiate a new object
+    public float lowerYThreshold = -6f; // Y value to remove the old object
+    public float duration = 1; // Duration to wait before checking positions again
+    private readonly float ZAxisValue = 25f; // Constant z-axis value
 
-    private List<GameObject> activeObjects = new List<GameObject>();
+    private List<GameObject> ActiveObjects = new List<GameObject>();
+    private GameObject LastActiveObject;
 
     void Awake()
     {
@@ -28,9 +28,9 @@ public class DistantPlanetsController : MonoBehaviour, IBackgroundController
 
   public void InitiateScrolling()
   {
-    if (activeObjects.Count > 0)
+    if (ActiveObjects.Count > 0)
     {
-      foreach (var obj in activeObjects)
+      foreach (var obj in ActiveObjects)
       {
         obj.transform.position += Vector3.down * scrollSpeed * Time.deltaTime;
       }
@@ -40,15 +40,17 @@ public class DistantPlanetsController : MonoBehaviour, IBackgroundController
     public void CheckAndAdd()
     {
         // Check if we need to add a new object
-        if (activeObjects.Count == 0 || activeObjects[activeObjects.Count - 1].transform.position.y <= upperYThreshold)
+        if (ActiveObjects.Count == 0 || LastActiveObject.transform.position.y <= upperYThreshold)
         {
             Vector3 newPosition = new Vector3(
                     Random.Range(-6.5f, 6.5f), // Adjust the range as needed
-                    Random.Range(5.5f, 13f), // Adjust the range as needed
-                    zAxisValue
+                    Random.Range(5.5f, 6.5f), // Adjust the range as needed
+                    ZAxisValue
                 );
 
             var newObject = Instantiate(AssetManager.DistantPlanetPrefab, newPosition, Quaternion.identity);
+            LastActiveObject = newObject;
+            ActiveObjects.Add(newObject);
 
             // Randomly choose a sprite and apply it to the new object
             var randomSprite = AssetManager.DistantPlanetSprites[Random.Range(0, AssetManager.DistantPlanetSprites.Count)];
@@ -61,15 +63,13 @@ public class DistantPlanetsController : MonoBehaviour, IBackgroundController
             {
                 Debug.LogError("DistantPlanetPrefab does not have a SpriteRenderer component.");
             }
-
-            activeObjects.Add(newObject);
         }
 
         // Check if we need to remove the old object
-        if (activeObjects.Count > 0 && activeObjects[0].transform.position.y <= lowerYThreshold)
+        if (ActiveObjects.Count > 0 && ActiveObjects[0].transform.position.y <= lowerYThreshold)
         {
-            Destroy(activeObjects[0]);
-            activeObjects.RemoveAt(0);
+            Destroy(ActiveObjects[0]);
+            ActiveObjects.RemoveAt(0);
         }
     }
 
